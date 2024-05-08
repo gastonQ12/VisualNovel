dd76b1397bb942395a65466c091a982412af84f8
gastonQ12 <162461760+gastonQ12@users.noreply.github.com>
2024-05-08 16:10:21 -0300

    Interfaz inGame
    Click detectado

---
+++ PPT/Script.js
@@ -0,0 +1,18 @@
+
+let aux;
+const dialogos=["sisi", "ñan ñam"];
+
+for (const aux of dialogos) {
+    document.addEventListener("click", a);
+  }
+
+/*
+dialogos.forEach(aux => {
+    document.removeEventListener("click", a);
+    document.
+});
+*/
+function a(event, aux) {
+    document.write(aux);
+    aux++;
+}
