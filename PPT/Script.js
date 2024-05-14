4d706eba6da3f42c0c36c6c3897d5f1e7d965492
gastonQ12 <162461760+gastonQ12@users.noreply.github.com>
2024-05-14 15:14:36 -0300

    mostrar texto(funcionando)

--- PPT/Script.js
+++ PPT/Script.js
@@ -1,18 +1,53 @@
-
-let aux;
-const dialogos=["sisi", "ñan ñam"];
-
+/*
 for (const aux of dialogos) {
     document.addEventListener("click", a);
-  }
+}
 
-/*
+
 dialogos.forEach(aux => {
     document.removeEventListener("click", a);
     document.
 });
-*/
+
 function a(event, aux) {
-    document.write(aux);
+}
+
+var elementos = document.querySelectorAll('*');
+
+elementos.forEach(function(elemento) {
+    elemento.addEventListener('click', function(event) {
+        console.log('Se ha hecho clic en el elemento:', elemento);
+        document.write("si");
+    });
+});
+
+
+
+
+while(aux<=3){
+    var elementos = document.querySelector("#output");
+    console.log(elementos);
+    mostrarTexto();
     aux++;
+    console.log(aux);
+};
+
+
+function mostrarTexto(){
+    elementos.addEventListener('click', function(event) {
+    console.log('Se ha hecho clic en el elemento:', elementos);
+    elementos.textContent = `sisi`;
+    }
+)
+};
+*/
+
+//Click para cambiar de dialogo
+let aux = 0;
+const dialogos=["sisi", "ñan ñam"];
+
+document.addEventListener('click', editarTexto);
+function editarTexto(event){
+    document.getElementById("output").textContent = dialogos[aux];
+    aux++
 }
