52c4a485c1ed02f52d4d72dd81bc4fdc2869a137
gastonQ12 <162461760+gastonQ12@users.noreply.github.com>
2024-05-22 15:36:48 -0300

    encontrar pista

--- minijuego buscar pistas/script.js
+++ minijuego buscar pistas/script.js
@@ -1,3 +1,11 @@
 
-const prueba = document.getElementById();
+var prueba = document.getElementById("prueba1");
 
+prueba.addEventListener('click', encontrarPista);
+
+function encontrarPista(event){
+
+    
+    alert('pista encontrada');
+    
+}
