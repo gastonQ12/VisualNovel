0966520a4027cf0106d8388dfc389fc009162d9d
gastonQ12 <162461760+gastonQ12@users.noreply.github.com>
2024-06-05 14:59:47 -0300

    modificar cursor

--- minijuego buscar pistas/script.js
+++ minijuego buscar pistas/script.js
@@ -2,6 +2,13 @@
 var prueba = document.getElementById("prueba1");
 
 prueba.addEventListener('click', encontrarPista);
+prueba.addEventListener('mouseenter', cursorEntrar)
+
+function cursorEntrar(event){
+
+    prueba.style.cursor='url(imagenes/cursor/)';
+    
+}
 
 function encontrarPista(event){
 
