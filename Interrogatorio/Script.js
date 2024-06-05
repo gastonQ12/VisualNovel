0ed727df7ae346f5dfd4f9adae3cd94350b6177c
gastonQ12 <101225983+Kalh2020@users.noreply.github.com>
2024-06-05 17:00:21 -0300

    Subida de archivo
    subida

--- Interrogatorio/Script.js
+++ Interrogatorio/Script.js
@@ -67,81 +67,116 @@ function editarTexto(event){
     }
 }
 */
-
-
-
 let aux = 0;
-const dialogos=['1.sisi', '1.ñan ñam', '2.ewfa', '1.mama que dice'];//  dialogos
-const nombrePj=["sujeto 1", "sujeto 2"];                            // nombre de los personajes
+document.body.onload = function () {
+    var leer = getCookie("progresoDialogo");
+    if (leer !== "") {
+        aux = parseInt(leer) - 1;
+        mostrarDialogos(aux); // Mostrar el diálogo guardado
+    }
+}
+const dialogos = ['1.sisi', '1.ñan ñam', '2.ewfa', '1.mama que dice'];//  dialogos
+const nombrePj = ["sujeto 1", "sujeto 2"];                            // nombre de los personajes
 let size = dialogos.length; //tamaño de la lista de dialogos
 var boxD = document.getElementById('cuadroDialogo');
 
 boxD.addEventListener('click', editarTexto);
 
-function editarTexto(event){    
-    if(aux<=size-1){
+
+
+function editarTexto(event) {
+    if (aux <= size - 1) {
         //ocultar los botones 
         document.getElementById("botonOpcion").setAttribute('hidden', '');
         document.getElementById("botonOpcion2").setAttribute('hidden', '');
 
         //mostrar dialogos
         mostrarDialogos(aux);
+        boxD.addEventListener('click', crearCookies);
         aux++
+
     }
-    else{
+    else {
         //mostrar botones y desactivar el dialogo
         var op1 = document.getElementById("botonOpcion").removeAttribute("hidden");
         var op2 = document.getElementById("botonOpcion2").removeAttribute("hidden");
 
     }
 }
-function libretaAnotar (  ){
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return unescape(c.substring(name.length, c.length));
+
+    }
+    return "";
+}
+
+function crearCookies(e) {
+
+
+    if (!e) e = window.event;
+    if (e.target.id == "cuadroDialogo") {
+        var cookies = document.cookie = "progresoDialogo=" + aux;
+        console.log(cookies);
+        
+    }
+}
+function libretaAnotar() {
     var papel = document.getElementById("")
-    document.getElementById("botonOpcion").addEventListener("click", function(){
+    document.getElementById("botonOpcion").addEventListener("click", function () {
         let pista1 = document.getElementById("pista1");
         pista1.style.opacity = 100 + "%";
     });
-    document.getElementById("botonOpcion2").addEventListener("click", function(){
-        
+    document.getElementById("botonOpcion2").addEventListener("click", function () {
+
     });
-    
+
 }
 
-function mostrarDialogos( auxiliar ){
+function mostrarDialogos(auxiliar) {
 
     //extrae la posicion del personaje
-    let pjHablando = dialogos[auxiliar].substring(0,1)-1;
+    let pjHablando = dialogos[auxiliar].substring(0, 1) - 1;
     console.log(pjHablando);
-    
+
+    console.log(auxiliar);
+
+
+
     //muestra el nombre, barrita y el dialogo
     document.getElementById("LineaDialogo").removeAttribute("hidden");
-    document.getElementById("PJname").textContent= nombrePj[pjHablando];
-    
+    document.getElementById("PJname").textContent = nombrePj[pjHablando];
+
     document.getElementById("output").textContent = dialogos[aux].substring(2);
     MostrarPjH(parseInt(pjHablando));
-}   
+}
 
 
 
-function MostrarPjH(lado){
+function MostrarPjH(lado) {
 
-    switch(lado){
+    switch (lado) {
         case 0:
             console.log("entro 0");
-        document.getElementById("izquierda").style.filter = "brightness(20%)";
-        document.getElementById("derecha").style.filter = "brightness(100%)";
-        
-        document.getElementById("izquierda").style.zIndex = "1";
-        document.getElementById("derecha").style.zIndex = "0";
-        break;
+            document.getElementById("izquierda").style.filter = "brightness(20%)";
+            document.getElementById("derecha").style.filter = "brightness(100%)";
+
+            document.getElementById("izquierda").style.zIndex = "1";
+            document.getElementById("derecha").style.zIndex = "0";
+            break;
         case 1:
-        console.log("entro 1");
-        document.getElementById("izquierda").style.filter = "brightness(100%)";
-        document.getElementById("derecha").style.filter = "brightness(20%)";
-        
-        document.getElementById("izquierda").style.zIndex = "0";
-        document.getElementById("derecha").style.zIndex = "1";
-        break;
-        }
+            console.log("entro 1");
+            document.getElementById("izquierda").style.filter = "brightness(100%)";
+            document.getElementById("derecha").style.filter = "brightness(20%)";
+
+            document.getElementById("izquierda").style.zIndex = "0";
+            document.getElementById("derecha").style.zIndex = "1";
+            break;
+    }
 
 }
