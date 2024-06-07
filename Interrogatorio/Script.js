1377ec3bc507a98f34e0f6c3857b2c279e20f4b3
Laureano Furno <101225983+Kalh2020@users.noreply.github.com>
2024-06-07 19:59:23 -0300

    Subida nueva
    Subida nueva

--- Interrogatorio/Script.js
+++ Interrogatorio/Script.js
@@ -4,20 +4,34 @@ document.body.onload = function () {
     if (leer !== "") {
         aux = parseInt(leer) - 1;
         mostrarDialogos(aux); // Mostrar el diálogo guardado
-        cambiarSrc(aux);
+        
     }
+    var fondo = getCookie("fondo");
+    body.style.backgroundImage = fondo;
 }
 
 
 const dialogos = ['1.¿De que se trata?', '2.Un asesinato, dicen que la victima es un taxista. Estiman que ocurrió esta mañana.',
-    '1.hmm, sabes no estamos teniendo mucho trabajo, podríamos echarles una mano con esto.',
+    '1.hmm, sabes no estamoFs teniendo mucho trabajo, podríamos echarles una mano con esto.',
     '2.No lo sé, seguramente se trate de un simple intento de atraco, no deberíamos perder el tiempo con este tipo de casos.',
     '1.Vamos, si de eso se trata no tardaremos mucho, no es como si tuviéramos mucho que hacer de todos modos.',
     '2.Está bien',
     '1.Buen día muchachos, diganme quien de ustedes es el que encontró el cuerpo',
     '3.Yo, señor.',
     '1.Ok, dime, no tocaste el cuerpo, ¿Cierto?',
-    '3.No, señor. Solo la tapa del contenedor, el cuerpo sigue ahí.',]; //  dialogos (Lau: Haganlo prolijo si agregan mas dialgos, asi no nos mareamos.)
+    '3.No, señor. Solo la tapa del contenedor, el cuerpo sigue ahí.',
+    '1.Perfecto. Ahora, necesito que me cuentes todo lo que sabes.',
+    '3.Bueno, solo hacía mi recorrido cuando note un taxi estacionado en medio de una calle desolada.',
+    '3.No le di mucha importancia, creí que solo era un taxista haciendo horas extra tratando de descansar un poco por lo que solo iba a seguir caminando cuando note que había dejado la puerta levemente abierta. ',
+    '3.Cuando me asome para avisarle al taxista me encontré con la sangrienta escena. El asiento delantero estaba lleno de manchas de sangre, entre un poco en pánico, creía que el culpable podría estar cerca. ',
+    '3.Note unas pequeñas gotas de sangre que me llevaron hasta el cuerpo.',
+    '1.Ya veo, le echaré un vistazo al lugar luego, ahora, ayudenme a sacar el cuerpo del contenedor.',
+    '1.',
+    '3.',
+    '1.',
+    '3.',
+    '1.',
+    '3.']; //  dialogos (Lau: Haganlo prolijo si agregan mas dialgos, asi no nos mareamos.)
 
 const nombrePj = ["(Protagonista)", "Ayudante", "Jhon Browns"];                            // nombre de los personajes
 let size = dialogos.length; //tamaño de la lista de dialogos
@@ -35,7 +49,7 @@ function editarTexto(event) {
         mostrarDialogos(aux);
         boxD.addEventListener('click', crearCookies);
         aux++
-        cambiarSrc(aux);
+        cambiarFondo(aux);
     }
     else {
         //mostrar botones y desactivar el dialogo
@@ -44,22 +58,7 @@ function editarTexto(event) {
 
     }
 }
-function cambiarSrc(aux) {
-    var imagen = document.getElementById('izquierda');
-    console.log('viva: ' + aux);
-    if (aux >= 7) {
 
-        if (imagen) {
-
-            imagen.src = 'imagenes/policia.png';
-            imagen.addEventListener('click', imagen.onload);
-
-        } else {
-            console.error('La imagen con el id "izquierda" no se encontró.');
-        }
-    }
-
-}
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
@@ -73,8 +72,6 @@ function getCookie(cname) {
 }
 
 function crearCookies(e) {
-
-
     if (!e) e = window.event;
     if (e.target.id == "cuadroDialogo") {
         var cookies = document.cookie = "progresoDialogo=" + aux;
@@ -108,9 +105,36 @@ function mostrarDialogos(auxiliar) {
 
     document.getElementById("output").textContent = dialogos[aux].substring(2);
     MostrarPjH(parseInt(pjHablando));
+    cambiarSrc(parseInt(pjHablando));
 }
 
+function cambiarSrc(aux) {
+    var imagen = document.getElementById('izquierda');
+    switch (aux) {
+        case 2:
+            imagen.src = 'imagenes/policia.png';
+            imagen.addEventListener('click', imagen.onload);
+            break;
+        case 1:
+            imagen.src = 'imagenes/ayudante.png';
+            imagen.addEventListener('click', imagen.onload);
+            break;
+    }
+}
 
+function cambiarFondo(aux){
+    var fondo = document.getElementById('body');
+    switch (aux){
+        case 8:
+            var fondoGuardado = 'url(imagenes/prueba.png)'
+            fondo.style.backgroundImage = fondoGuardado;
+            document.cookie = "fondo=" + fondoGuardado;
+            break;
+
+    }
+    
+    
+}
 
 function MostrarPjH(lado) {
 
