eefb8c9169ede560efaf4f451e645eb505987624
gastonQ12 <162461760+gastonQ12@users.noreply.github.com>
2024-07-07 12:32:51 -0300

    script.js

--- back-end quick event/sctipt.js
+++ back-end quick event/sctipt.js
@@ -1,5 +1,5 @@
 
-
+inicioMinijuago()
 
 // let tecla = letraRandom();
 // document.write(tecla);
@@ -22,20 +22,23 @@ function inicioMinijuago(){
 
     let tecla = letraRandom();
     document.write(tecla);
-    ingresarTecla(tecla);
-    
-    function ingresarTecla(tecla){
-        document.addEventListener('keydown', function(event) {
-            if(tecla==event.key){alert("Tecla correcta")}
-            else{alert("que tocaba el pibe")}
-        })
-    }
-    
-    function letraRandom() {
-        const letras = ['a', 's', 'd', 'w', 'e', 'q', 'f', 'z', 'x', 'c'];
-        const indexRandom = Math.floor(Math.random() * letras.length);
-        return letras[indexRandom];
+    if(ingresarTecla(tecla)){
+        alert("Tecla correcta")
     }
 
 
+function ingresarTecla(tecla){
+    document.addEventListener('keydown', function(event) {
+        if(tecla==event.key){ return true;}
+        else{
+            else{
+                alert("que tocaba el pibe");}
+            }return false;}
+    })
+}
+
+function letraRandom() {
+    const letras = ['a', 's', 'd', 'w', 'e', 'q', 'f', 'z', 'x', 'c'];
+    const indexRandom = Math.floor(Math.random() * letras.length);
+    return letras[indexRandom];
 }
