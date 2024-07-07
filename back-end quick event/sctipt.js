76bfe82ee40dbc8a32421ba85f3e4de5e67176cb
gastonQ12 <162461760+gastonQ12@users.noreply.github.com>
2024-07-07 11:40:03 -0300

    update js

--- back-end quick event/sctipt.js
+++ back-end quick event/sctipt.js
@@ -1,2 +1,41 @@
-document.addEventListener('keydown', function(event) {
-            alert(' tecla: ' + event.key);})
+
+
+
+// let tecla = letraRandom();
+// document.write(tecla);
+// ingresarTecla(tecla);
+
+// function ingresarTecla(tecla){
+//     document.addEventListener('keydown', function(event) {
+//         if(tecla==event.key){alert("Tecla correcta")}
+//         else{alert("que tocaba el pibe")}
+//     })
+// }
+
+// function letraRandom() {
+//     const letras = ['a', 's', 'd', 'w', 'e', 'q', 'f', 'z', 'x', 'c'];
+//     const indexRandom = Math.floor(Math.random() * letras.length);
+//     return letras[indexRandom];
+// }
+
+function inicioMinijuago(){
+
+    let tecla = letraRandom();
+    document.write(tecla);
+    ingresarTecla(tecla);
+    
+    function ingresarTecla(tecla){
+        document.addEventListener('keydown', function(event) {
+            if(tecla==event.key){alert("Tecla correcta")}
+            else{alert("que tocaba el pibe")}
+        })
+    }
+    
+    function letraRandom() {
+        const letras = ['a', 's', 'd', 'w', 'e', 'q', 'f', 'z', 'x', 'c'];
+        const indexRandom = Math.floor(Math.random() * letras.length);
+        return letras[indexRandom];
+    }
+
+
+}
