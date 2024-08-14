bcbabddcefb42baf4deabf771f46cbdb8255678e
gastonQ12 <162461760+gastonQ12@users.noreply.github.com>
2024-08-14 17:34:18 -0300

    avances js

--- quickTimeCINE/codigoGOD.js
+++ quickTimeCINE/codigoGOD.js
@@ -8,13 +8,16 @@ const shadow = document.querySelector('.shadow');
 let vidaEnemigo = 100 ;
 let vidaJugador = 100 ;
 var aux = true;
-txt.textContent = tecla;
+botonTexto.textContent = tecla.toUpperCase();
 //obtener cuando la animacion de la variable termina
 
+ingresarTecla(tecla);
 shadow.addEventListener('animationiteration', function(event) {
-
-    ingresarTecla(tecla);
-
+    boton.style.backgroundColor = "red"
+    vidaJugador = vidaJugador - 10;
+    barraVidaJugador.style.background = `linear-gradient(to right, 
+    #AE0909 ${vidaJugador}%, 
+    white ${vidaJugador - 100}%)`;
 });
 
 
@@ -27,21 +30,24 @@ shadow.addEventListener('animationiteration', function(event) {
 
 //funcion para ingresar la tecla
 function ingresarTecla(tecla){
+    botonTexto.textContent = tecla.toUpperCase();
     document.addEventListener('keydown', function(event) {
-        if(tecla==event.key.toUpperCase()){
+        if(tecla == event.key){
             boton.style.backgroundColor = "green" 
-            vidaEnemigo = vidaEnemigo - 10;
-            barraVidaEnemigo.style.background = `linear-gradient(to right, 
-            #AE0909 ${vidaEnemigo}%, 
-            white ${vidaEnemigo - 100}%)`;
-            
+            vidaEnemigo = vidaEnemigo - 2;
+            barraVidaEnemigo.style.background = `linear-gradient(to right, #AE0909 ${vidaEnemigo}%, white ${vidaEnemigo - 100}%)`;            
+            tecla = letraRandom()   
+            botonTexto.textContent = tecla.toUpperCase();
         }
         else{
             boton.style.backgroundColor = "red"
-            vidaJugador = vidaJugador - 10;
+            vidaJugador = vidaJugador - 2;
             barraVidaJugador.style.background = `linear-gradient(to right, 
             #AE0909 ${vidaJugador}%, 
             white ${vidaJugador - 100}%)`;
+            tecla = letraRandom()   
+            botonTexto.textContent = tecla.toUpperCase();
+        
         }
     })
 
@@ -49,7 +55,7 @@ function ingresarTecla(tecla){
 
 //funcion para generar la letra random
 function letraRandom() {
-    const letras = ['A', 'S', 'D', 'W', 'E', 'Q', 'F', 'Z', 'X', 'C'];
+    const letras = ['a', 's', 'd', 'w', 'e', 'q', 'f', 'z', 'x', 'c'];
     const indexRandom = Math.floor(Math.random() * letras.length);
     return letras[indexRandom];
 
