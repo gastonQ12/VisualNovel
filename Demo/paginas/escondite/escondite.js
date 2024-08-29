9ec193a9cafd85ade131faa293bb6efb6a22374b
voxaive <148632238+voxaive@users.noreply.github.com>
2024-08-29 09:50:46 -0300

    GuardadoEmergencia
    .

--- Demo/paginas/escondite/escondite.js
+++ Demo/paginas/escondite/escondite.js
@@ -1,31 +1,57 @@
-document.body.onload = function (){
-    
-}
-var cama = document.getElementById("escondite2");
-var cortinas = document.getElementById("escondite1");
-var balcon = document.getElementById("escondite3");
-var reloj = document.getElementById("reloj");
-var probabilidadSalvarse = 0;
-
-var bajar = 200000000;
-
-localStorage.setItem("Escondite1", 1);
-
-const intervalo = setInterval(() => {
-    reloj.style.background = `conic-gradient(red ${bajar}deg, white ${bajar}deg)`;
-    bajar -= 9;
-    if (bajar <= 0) {
-        clearInterval(intervalo);
-    }  
-    console.log(bajar);
-    if(bajar == 0){
-        volver();
+function proseguir() {
+    var prob = Math.floor(Math.random() * (100 - 10 + 1) + 10);
+    var aviso = document.getElementById('aviso');
+    aviso.style.display = 'none';
+    console.log(prob)
+    if(50 <= prob){
+        esconderse();
     }
-}, 200
-); 
+}
 
-function volver(event){
-    document.location.href="../../Pantalla Muerte/index.html";
+function esconderse(){
+    var cama = document.getElementById("escondite2");
+    var cortinas = document.getElementById("escondite1");
+    var balcon = document.getElementById("escondite3");
+    var reloj = document.getElementById("reloj");
+    var probabilidadSalvarse = 0;
+
+    var bajar = 360;
+
+    localStorage.setItem("Escondite1", 1);
+
+    const intervalo = setInterval(() => {
+        reloj.style.background = `conic-gradient(red ${bajar}deg, white ${bajar}deg)`;
+        bajar -= 9;
+        if (bajar <= 0) {
+            clearInterval(intervalo);
+        }
+        console.log(bajar);
+        if (bajar == 0) {
+            volver();
+        }
+    }, 200
+    );
+
+    cama.addEventListener('click', function () {
+        var probabilidadSalvarse = 75;
+
+        salvado(probabilidadSalvarse);
+    });
+
+    cortinas.addEventListener('click', function () {
+        var probabilidadSalvarse = 40;
+
+        salvado(probabilidadSalvarse);
+    });
+
+    balcon.addEventListener('click', function () {
+        var probabilidadSalvarse = 86;
+
+        salvado(probabilidadSalvarse);
+    });
+}
+function volver(event) {
+    document.location.href = "../../Pantalla Muerte/index.html";
 }
 
 var urlAnterior = new URL(window.location);
@@ -33,31 +59,13 @@ var rutaAnterior = urlAnterior.pathname;
 localStorage.removeItem('ub');
 localStorage.setItem('ub', rutaAnterior);
 
-cama.addEventListener('click', function(){ 
-    var probabilidadSalvarse = 75;
-
-    salvado(probabilidadSalvarse);
-});
-
-cortinas.addEventListener('click', function(){ 
-    var probabilidadSalvarse = 40;
-
-    salvado(probabilidadSalvarse);
-});
-
-balcon.addEventListener('click', function(){ 
-    var probabilidadSalvarse = 86;
-
-    salvado(probabilidadSalvarse);
-});
-
-function salvado(probabilidad){
+function salvado(probabilidad) {
     var random = Math.floor(Math.random() * (100 - 10 + 1) + 10);
     console.log("Random" + random);
     console.log("Probabilidad" + probabilidad);
-    if(probabilidad <= random){
+    if (probabilidad <= random) {
         alert("No te salvaste");
-    }else{
+    } else {
         alert("te salvaste");
         localStorage.removeItem("Escondite1");
     }
