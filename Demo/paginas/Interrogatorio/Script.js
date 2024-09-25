2076cc714fa9ca8c712964ccdaec2b2c719687c7
voxaive <148632238+voxaive@users.noreply.github.com>
2024-09-25 17:25:01 -0300

    Subo mejoras en la casa de Sophie

--- Demo/paginas/Interrogatorio/Script.js
+++ Demo/paginas/Interrogatorio/Script.js
@@ -7,7 +7,7 @@ const partidaAnti = localStorage.getItem('codigoPartida');
 document.body.onload = function () {
     var paginaAnterior = document.referrer;
     var valorPC = 1;
-    if(partidaA !== partidaAnti){
+    if (partidaA !== partidaAnti) {
         if (valorPC == 1) {
             eliminarCookie("progresoDialogo");
             eliminarCookie("boxD");
@@ -43,8 +43,8 @@ document.body.onload = function () {
         }
 
     }
-        
-    
+
+
     var urlAnterior = new URL(window.location);
     var rutaAnterior = urlAnterior.pathname;
     localStorage.removeItem('ub');
@@ -83,25 +83,25 @@ document.body.onload = function () {
     determinarPE();
     cambiarColorLabel();
     determinarPersonajeExistente();
-/*
-    var arrayConvertido = localStorage.getItem('EdgardM');
-    var nuevoArray = JSON.parse(arrayConvertido);
-
-    for(let i = 0; i < nuevoArray.length; i++){
-
-        let subArray = nuevoArray[i];
-        for(let s = 0; s < subArray[i]; s++)
-        if(subArray[s].EdgardM[0] === true){
-          
-            document.getElementById("LP1Si").style.backgroundColor="green";
-            document.getElementById("LP1No").style.backgroundColor="grey";
-        }else{
-            document.getElementById("LP1No").style.backgroundColor="Red";
-            document.getElementById("LP1Si").style.backgroundColor="grey";
-        
+    /*
+        var arrayConvertido = localStorage.getItem('EdgardM');
+        var nuevoArray = JSON.parse(arrayConvertido);
+    
+        for(let i = 0; i < nuevoArray.length; i++){
+    
+            let subArray = nuevoArray[i];
+            for(let s = 0; s < subArray[i]; s++)
+            if(subArray[s].EdgardM[0] === true){
+              
+                document.getElementById("LP1Si").style.backgroundColor="green";
+                document.getElementById("LP1No").style.backgroundColor="grey";
+            }else{
+                document.getElementById("LP1No").style.backgroundColor="Red";
+                document.getElementById("LP1Si").style.backgroundColor="grey";
+            
+            }
         }
-    }
-    */
+        */
 }
 
 var derecha = document.getElementById('derS');
@@ -119,58 +119,58 @@ function moverDer(event) {
 // document.getElementById('P1').textContent = localStorage.getItem("opacidadP1").key;
 
 function determinarPE() {
-    for(let i = 0; i < 4; i++){
+    for (let i = 0; i < 4; i++) {
         if (localStorage.getItem("OpacidadP1") !== null) {
-            var contP1 =document.getElementById('contP1_'+(i+1)).style.opacity = 100 + "%";
-        }else{
-            document.getElementById('contP1_'+(i+1)).style.opacity = 0 + "%";
+            var contP1 = document.getElementById('contP1_' + (i + 1)).style.opacity = 100 + "%";
+        } else {
+            document.getElementById('contP1_' + (i + 1)).style.opacity = 0 + "%";
         }
         if (localStorage.getItem("opacidadPCadaver") != null) {
-            document.getElementById('contP2_'+(i+1)).style.opacity = 100 + "%";
-        }else{
-            document.getElementById('contP1_'+(i+1)).style.opacity = 0 + "%";
+            document.getElementById('contP2_' + (i + 1)).style.opacity = 100 + "%";
+        } else {
+            document.getElementById('contP1_' + (i + 1)).style.opacity = 0 + "%";
         }
         if (localStorage.getItem("opacidadPBasurero") !== null) {
-            document.getElementById('contP3_'+(i+1)).style.opacity = 100 + "%";
-        }else{
-            document.getElementById('contP3_'+(i+1)).style.opacity = 0 + "%";
+            document.getElementById('contP3_' + (i + 1)).style.opacity = 100 + "%";
+        } else {
+            document.getElementById('contP3_' + (i + 1)).style.opacity = 0 + "%";
         }
         if (localStorage.getItem("opacidadPTaxi") !== null) {
-            document.getElementById('contP4_'+(i+1)).style.opacity = 100 + "%";
-        }else{
-            document.getElementById('contP4_'+(i+1)).style.opacity = 0 + "%";
+            document.getElementById('contP4_' + (i + 1)).style.opacity = 100 + "%";
+        } else {
+            document.getElementById('contP4_' + (i + 1)).style.opacity = 0 + "%";
         }
         if (localStorage.getItem("opacidadPRadio") !== null) {
-            document.getElementById('contP5_'+(i+1)).style.opacity = 100 + "%";
-        }else{
-            document.getElementById('contP5_'+(i+1)).style.opacity = 0 + "%";
+            document.getElementById('contP5_' + (i + 1)).style.opacity = 100 + "%";
+        } else {
+            document.getElementById('contP5_' + (i + 1)).style.opacity = 0 + "%";
 
         }
     }
-   
+
 }
 
 function determinarPersonajeExistente() {
-    for(let i = 0; i < 4; i++){
+    for (let i = 0; i < 4; i++) {
         if (localStorage.getItem("Sophie") !== null) {
-            var contP1 =document.getElementById('sophie').style.opacity = 100 + "%";
-        }else{
-            document.getElementById('sophie').style.display= 'none';
+            var contP1 = document.getElementById('sophie').style.opacity = 100 + "%";
+        } else {
+            document.getElementById('sophie').style.display = 'none';
         }
         if (localStorage.getItem("Henry") != null) {
             document.getElementById('henry').style.opacity = 100 + "%";
-        }else{
-            document.getElementById('henry').style.display= 'none';
+        } else {
+            document.getElementById('henry').style.display = 'none';
         }
         if (localStorage.getItem("Edgar") !== null) {
             document.getElementById('edgar').style.opacity = 100 + "%";
-        }else{
-            document.getElementById('edgar').style.display= 'none';
+        } else {
+            document.getElementById('edgar').style.display = 'none';
         }
         if (localStorage.getItem("Richard") !== null) {
             document.getElementById('richard').style.opacity = 100 + "%";
-        }else{
-            document.getElementById('richard').style.display= 'none';
+        } else {
+            document.getElementById('richard').style.display = 'none';
         }
     }
 }
@@ -181,13 +181,13 @@ function moverIzq(event) {
 var protagonista = localStorage.getItem('NombrePJ');
 const dialogos = [
     '  3 de febrero de 1910 a las 12:45 AM, Londres.',
-    '1. Buenas noches señora (Apellido), soy el detective '+ protagonista+ ', ¿Tiene un minuto?',
-    '2. Claro, adelante... pase. '+ mostrarSophie(),
+    '1. Buenas noches señora Petterson, soy el detective ' + protagonista + ', ¿Tiene un minuto?',
+    '2. Claro, adelante... pase. ' + mostrarSophie(),
     '2. Entonces, ¿Que sucede detective?',
     '1. Sera mejor que tome asiento, la noticia puede ser muy fuerte.',
     '2. Esta bien, acompañeme. ',
-    '1. Bueno, señora (Apellido), lamento informarle que su esposo, el señor (Apellido)',
-    '1. Debo informarle que su esposo, el Señor (Apellido), lamentablemente fallecio hace pocas horas',
+    '1. Bueno, señora Petterson, lamento informarle que su esposo, el señor Petterson',
+    '1. Debo informarle que su esposo, el Señor Petterson, lamentablemente fallecio hace pocas horas',
     '2. Oh... ¿Cuando...? ¿Como?, lo vi hace apenas unas horas...',
     '1. Murio en un callejon a pocas cuadras, la teoria es que fue emboscado, ¿Sabe de algun sujeto que le guarde rencor?',
     '2. Ah... no, el no era una mala persona o una que buscara conflictos innecesarios, era tierno, comprensivo y amoroso...',
@@ -195,11 +195,19 @@ const dialogos = [
     '2. ¿Que insinua detective?',
     '1. Necesito saber si hubo alguien fuera del matrimonio que podria tener celos, algun amante resentido.',
     '2. No detective, no ofenda nuestro matrimonio puro, eramos el uno para el otro',
-    '1. Bueno... necesito un indicio'
+    '1. Bueno... necesito saber si reconoce este simbolo',
+    '2. Emh... no, no que yo sepa, mis disculpas',
+    '1. ¿Y esta corbata?',
+    '2. Tampoco, no es ninguna de las corbatas de Michael',
+    '1. Umh... Disculpeme, ¿Podria traer un poco de agua o algo para beber?',
+    '2. Claro.',
+    '1.',
+    '1.',
+    '1.'
+
 ]; //  dialogos (Lau: Haganlo prolijo si agregan mas dialgos, asi no nos mareamos.)
 
-
-const nombrePj = [protagonista, "Sophie (Apellido)"];                            // nombre de los personajes
+const nombrePj = [protagonista, "Sophie Petterson"];                            // nombre de los personajes
 let size = dialogos.length; //tamaño de la lista de dialogos
 var boxD = document.getElementById('cuadroDialogo');
 
@@ -214,6 +222,7 @@ function editarTexto(event) {
         aux++
         boxD.addEventListener('click', crearCookies(aux));
         libretaAnotarComentarios(aux);
+        esconditeEvento(aux);
 
     }
     else {
@@ -253,10 +262,11 @@ function libretaAnotarComentarios(aux) {
 
 }
 
-function mostrarSophie(){
+function mostrarSophie() {
     localStorage.setItem("Sophie", true);
     determinarPersonajeExistente();
 }
+
 function mostrarDialogos(auxiliar) {
 
     //extrae la posicion del personaje
@@ -284,14 +294,14 @@ function cambiarSrc(auxs) {
             imagen.src = 'imagenes/SophieInd.png';
             imagen.addEventListener('click', imagen.onload);
             document.cookie = "pjHablando=" + 'imagenes/SophieInd.png';
-           /* 
-            if(aux >= 4 ){
-                imagen.src = 'imagenes/SophieTriste.jpg';
-                imagen.addEventListener('click', imagen.onload);
-                document.cookie = "pjHablando=" + 'imagenes/SophieTriste.jpg';
-            }
-            break;
-            */
+        /* 
+         if(aux >= 4 ){
+             imagen.src = 'imagenes/SophieTriste.jpg';
+             imagen.addEventListener('click', imagen.onload);
+             document.cookie = "pjHablando=" + 'imagenes/SophieTriste.jpg';
+         }
+         break;
+         */
     }
 }
 function pjOcultos(aux) {
@@ -396,28 +406,28 @@ function MostrarPjH(lado) {
 }
 
 
-function cambiarColorLabel(){
+function cambiarColorLabel() {
     var arrayConvertido = localStorage.getItem('Sospechosos');
     var nuevoArray = JSON.parse(arrayConvertido);
 
-    for(let i = 0; i <= nuevoArray.length; i++){
+    for (let i = 0; i <= nuevoArray.length; i++) {
         var ind = nuevoArray[i];
-        if (Array.isArray(ind)){
-            for(let g = 0; g <= ind.length; g++){
-                if(ind[g] === true){
-                    document.getElementById('LP'+(g+1)+'Si_'+(i+1)).style.backgroundColor="green";
-                    document.getElementById('LP'+(g+1)+'No_'+(i+1)).style.backgroundColor="grey";
-                }else if(ind[g] === false){
-                    document.getElementById('LP'+(g+1)+'No_'+(i+1)).style.backgroundColor="Red"; 
-                    document.getElementById('LP'+(g+1)+'Si_'+(i+1)).style.backgroundColor="gray";
-                }else if(ind[g] === null){
-                    document.getElementById('LP'+(g+1)+'No_'+(i+1)).style.backgroundColor="gray"; 
-                    document.getElementById('LP'+(g+1)+'Si_'+(i+1)).style.backgroundColor="gray";
+        if (Array.isArray(ind)) {
+            for (let g = 0; g <= ind.length; g++) {
+                if (ind[g] === true) {
+                    document.getElementById('LP' + (g + 1) + 'Si_' + (i + 1)).style.backgroundColor = "green";
+                    document.getElementById('LP' + (g + 1) + 'No_' + (i + 1)).style.backgroundColor = "grey";
+                } else if (ind[g] === false) {
+                    document.getElementById('LP' + (g + 1) + 'No_' + (i + 1)).style.backgroundColor = "Red";
+                    document.getElementById('LP' + (g + 1) + 'Si_' + (i + 1)).style.backgroundColor = "gray";
+                } else if (ind[g] === null) {
+                    document.getElementById('LP' + (g + 1) + 'No_' + (i + 1)).style.backgroundColor = "gray";
+                    document.getElementById('LP' + (g + 1) + 'Si_' + (i + 1)).style.backgroundColor = "gray";
                 }
             }
         }
-        
-        
+
+
     }
 }
 
@@ -425,17 +435,17 @@ function updateStatus() {
     var arrayConvertido = localStorage.getItem('Sospechosos');
     var nuevoArray = JSON.parse(arrayConvertido);
 
-    for(let i = 0; i < nuevoArray.length; i++){
+    for (let i = 0; i < nuevoArray.length; i++) {
         var ind = nuevoArray[i];
 
         if (Array.isArray(ind)) {
-            for(let g = 0; g < ind.length; g++){
-                let marcarEdgardP1Si = document.getElementById('P'+(g+1)+'Si_'+(i+1));
-                let marcarEdgardP1No = document.getElementById('P'+(g+1)+'No_'+(i+1));
-                if (marcarEdgardP1Si && marcarEdgardP1Si.checked){
+            for (let g = 0; g < ind.length; g++) {
+                let marcarEdgardP1Si = document.getElementById('P' + (g + 1) + 'Si_' + (i + 1));
+                let marcarEdgardP1No = document.getElementById('P' + (g + 1) + 'No_' + (i + 1));
+                if (marcarEdgardP1Si && marcarEdgardP1Si.checked) {
                     ind[g] = true;
                 }
-                else if (marcarEdgardP1No && marcarEdgardP1No.checked){
+                else if (marcarEdgardP1No && marcarEdgardP1No.checked) {
                     ind[g] = false
                 }
             }
@@ -448,3 +458,92 @@ function updateStatus() {
 document.querySelectorAll('input[type="radio"]').forEach(radio => {
     radio.addEventListener('change', updateStatus);
 });
+
+
+/* Esconderse */
+function esconditeEvento(aux) {
+    if (aux == 22) {
+        sacarTodo();
+        esconderse();
+        clearInterval(relojTodoTiempo);
+    }
+    function sacarTodo() {
+        let audioTic = document.getElementById('audioTic');
+        audioTic.play();
+        let iconosE = document.getElementById('iconosE');
+        let reloj = document.getElementById('reloj');
+        let cuadroDialogo = document.getElementById('cuadroDialogo');
+        let imgPjs = document.getElementById('imagenesPJ');
+        imgPjs.style.display = 'none';
+        cuadroDialogo.style.display = 'none';
+        iconosE.style.display = 'block';
+
+        reloj.style.display = 'block';
+    }
+    function ponerTodo() {
+        let iconosE = document.getElementById('iconosE');
+        let reloj = document.getElementById('reloj');
+        let cuadroDialogo = document.getElementById('cuadroDialogo');
+        cuadroDialogo.style.display = 'flex';
+        iconosE.style.display = 'block';
+        reloj.style.display = 'block';
+    }
+    function esconderse() {
+        var cama = document.getElementById("escondite2");
+        var cortinas = document.getElementById("escondite1");
+        var balcon = document.getElementById("escondite3");
+        var reloj = document.getElementById("reloj");
+        var probabilidadSalvarse = 0;
+
+        var bajar = 360;
+
+        localStorage.setItem("Escondite1", 1);
+
+        const intervalo = setInterval(() => {
+            reloj.style.background = `conic-gradient(red ${bajar}deg, white ${bajar}deg)`;
+            bajar -= 9;
+            if (bajar <= 0) {
+                clearInterval(intervalo);
+            }
+            console.log(bajar);
+            if (bajar == 0) {
+                volver();
+            }
+        }, 800
+        );
+
+        cama.addEventListener('click', function () {
+            var probabilidadSalvarse = 75;
+
+            salvado(probabilidadSalvarse);
+        });
+
+        cortinas.addEventListener('click', function () {
+            var probabilidadSalvarse = 40;
+
+            salvado(probabilidadSalvarse);
+        });
+
+        balcon.addEventListener('click', function () {
+            var probabilidadSalvarse = 86;
+
+            salvado(probabilidadSalvarse);
+        });
+    }
+
+    function volver(event) {
+        document.location.href = "../../Pantalla Muerte/index.html";
+    }
+
+    function salvado(probabilidad) {
+        var random = Math.floor(Math.random() * (100 - 10 + 1) + 10);
+        console.log("Random" + random);
+        console.log("Probabilidad" + probabilidad);
+        if (probabilidad <= random) {
+            alert("No te salvaste");
+        } else {
+            alert("te salvaste");
+            localStorage.removeItem("Escondite1");
+        }
+    }
+}
